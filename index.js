5c156c63dedbbc36b590a94e5812a913b1a771cb
mstsurnalyakter <surnalyms@gmail.com>
2024-05-08 19:31:10 +0600

    update mongodb uri

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ app.use(express.json());
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jimwvxl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -49,5 +50,5 @@ app.get('/', (req, res) =>{
 })
 
 app.listen(port, () =>{
-    console.log(`ema john server is running on port: ${port}`);
+    console.log(`ema john server is running on port: http://localhost:${port}`);
 })
