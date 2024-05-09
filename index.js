246f68c025eecf0ef49d8721d19e1ece5d7944f4
mstsurnalyakter <surnalyms@gmail.com>
2024-05-09 08:17:57 +0600

    fix order page error

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jimwvxl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -35,6 +35,18 @@ async function run() {
         .limit(size)
         .toArray();
 
+      res.send(result);
+    });
+
+    app.post("/productByIds",async(req,res)=>{
+      const ids = req.body;
+      const idsWithObjectId = ids.map(id=> new ObjectId(id))
+      const query = {
+        _id:{
+          $in:idsWithObjectId
+        }
+      }
+      const result = await productCollection.find(query).toArray();
       res.send(result);
     });
 
